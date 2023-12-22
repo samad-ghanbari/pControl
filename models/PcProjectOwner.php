f0d6cda3767f3cce9005ba859aab4e822e201af6
Samad Ghanbari <ghanbari.samad@gmail.com>
2023-12-22 13:26:16 +0330

    add model project_owner

---
+++ models/PcProjectOwner.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "pc.project_owner".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $project_id
+ */
+class PcProjectOwner extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'pc.project_owner';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'project_id'], 'required'],
+            [['user_id', 'project_id'], 'default', 'value' => null],
+            [['user_id', 'project_id'], 'integer'],
+            [['project_id'], 'exist', 'skipOnError' => true, 'targetClass' => PcProjects::className(), 'targetAttribute' => ['project_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => PcUsers::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'project_id' => 'Project ID',
+        ];
+    }
+}
